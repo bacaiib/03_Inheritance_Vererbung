dbf2512d56462e66fcb3c71add9679546cf4fadf
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-29 09:55:33 +0100

    Add files via upload

---
+++ com/cc/java/Cat.java
@@ -0,0 +1,49 @@
+package com.cc.java;
+
+public class Cat {
+
+  private String name;
+  private String furColor;
+  private int age; 
+  private int counter;
+
+  public Cat(String name, String furColor, int age) {
+    this.name = name;
+    this.furColor = furColor;
+    this.age = age;
+  }
+
+  public String getStringAttributes(String op) {
+    switch (op) {
+      case "#name":
+        return name;
+      case "#color":
+        return furColor;
+      default:
+        return "ERROR!";
+    }
+  }
+
+  public String getAge() {
+    return checkCompliance();
+  }
+
+  private String checkCompliance(){
+
+    counter++;
+
+    switch (counter) {
+      case 1:
+        return "This is an inappropriate question!";
+      case 2:
+        return "I've told you once!";
+      case 3:
+        return "Talk to the hand!";
+      default:
+        return "1#!?&&%";
+    }
+    
+  }
+
+}
+  

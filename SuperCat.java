f75f93f711e47591a27b7c2e054155a2c74e8f57
bacaiib <roman-frommhold@gmx.net>
2024-01-29 10:55:52 +0100

    Supercat ersteschritte

---
+++ SuperCat.java
@@ -0,0 +1,26 @@
+public class SuperCat {
+
+    String name;
+    String furColor;
+    int age;
+
+
+    public SuperCat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+    public String getStringAttributes(String op) {
+        switch (op) {
+          case "#name":
+            return name;
+          case "#color":
+            return furColor;
+          default:
+            return "ERROR!";
+        }
+      }
+    
+
+}
